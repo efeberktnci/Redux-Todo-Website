5f0cb23eee82bcbd7461aff9cd4b46250be87eb6
efeberktnci <efeberktanci@gmail.com>
2024-01-10 14:04:21 +0300

    Added delete function

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,12 +1,25 @@
+import { useDispatch } from "react-redux";
+
 const TodoCard = ({ todo }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+   dispatch({
+    type:"REMOVE_TODO",
+    payload: todo.id,
+   })
+  };
+
   return (
-    <div>
+    <div className="border rounded-5 shadow shadow-lg p-4 my-5">
       <h5>{todo.text}</h5>
       <h6>{todo.is_done ? "Tamamlandı" : "Devam Ediyor"}</h6>
       <p>{todo.created_at}</p>
-      <button>Edit</button>
-      <button>Complete</button>
-      <button>Delete</button>
+      <button className="btn btn-primary">Edit</button>
+      <button className="btn btn-success mx-3">Complete</button>
+      <button onClick={handleDelete} className="btn btn-danger">
+        Delete
+      </button>
     </div>
   );
 };
--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -5,17 +5,22 @@ const initialState = {
 }
 
 
-const todoReducer = (state = initialState ,action) => {
+const todoReducer = (state = initialState, action) => {
     switch(action.type){
         case "ADD_TODO":
             const tempTodos = state.todos.concat(action.payload);
 
             return {...state, todos: tempTodos }
             
-        case "REMOVE_TODO":
-            return "BOŞ";
+        case "REMOVE_TODO": 
+            const filtered = state.todos.filter(
+             (todo) => todo.id !== action.payload)
+            return {...state, todos: filtered};
+
+
             default:
                 return state;
+        
     }
 };
 

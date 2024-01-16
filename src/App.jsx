e2d8662e4e98a201156a21fb2a8cb3a2f8902817
efeberktnci <efeberktanci@gmail.com>
2024-01-16 12:59:55 +0300

    created required axios api environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AddForm from './components/AddForm'
 import ListTodos from './components/ListTodos'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from "axios"
+
+axios.defaults.baseURL = "http://localhost:3040"
+
 
 const App = () => {
+
+  useEffect(() => {
+    axios.get("/todos").then((res) => console.log(res.data))
+  },[]);
+
   return (
    <div className='bg-dark vh-100 vw-100'>
     <div className='container p-5'>
